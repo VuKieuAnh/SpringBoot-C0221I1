8eb8f9146fad755c9a01e191f881173f49b0c334
kieuanh <vukieuanh.hnue@gmail.com>
2021-06-30 09:41:08 +0700

    add config security

--- src/main/java/com/codegym/democ0221i1springboot/config/AppSecConfig.java
+++ src/main/java/com/codegym/democ0221i1springboot/config/AppSecConfig.java
@@ -1,6 +1,8 @@
 package com.codegym.democ0221i1springboot.config;
 
 
+import com.codegym.democ0221i1springboot.service.userservice.IAppUserservice;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,26 +11,37 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 public class AppSecConfig extends WebSecurityConfigurerAdapter {
-    @Bean
-    public UserDetailsService userDetailsService() {
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//
+//        User.UserBuilder users = User.withDefaultPasswordEncoder();
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        manager.createUser(users.username("user").password("123456").roles("KA").build());
+//        manager.createUser(users.username("admin").password("123456").roles("ADMIN").build());
+//        return manager;
+//
+//    }
 
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(users.username("user").password("123456").roles("KA").build());
-        manager.createUser(users.username("admin").password("123456").roles("ADMIN").build());
-        return manager;
+    @Autowired
+    private IAppUserservice appUserservice;
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService((UserDetailsService) appUserservice).passwordEncoder(
+                NoOpPasswordEncoder.getInstance()
+        );
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/").permitAll()
                 .and()
-                .authorizeRequests().antMatchers("/user").hasRole("KA")
+                .authorizeRequests().antMatchers("/user").hasRole("user")
                 .and()
                 .authorizeRequests().antMatchers("/tasks/**").hasRole("ADMIN")
                 .and()
